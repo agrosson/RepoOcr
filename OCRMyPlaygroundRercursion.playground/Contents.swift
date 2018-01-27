0b98ba1a3a3a0251fc61eb6a61afd2589ad9c71e
agrosson <agrosson@gmail.com>
2018-01-27 16:05:30 +0100

    Commit initial

---
+++ OCRMyPlaygroundRercursion.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// Example recursive function
+// Transform parameter (Int) into binary number (String)
+//
+
+var str = ""
+func binaryF(n:Int) -> String{
+    
+    // Best case n=0 and n=1 binary representation is the same respectively 0 and 1
+    if n<2 {
+        //  print(n)
+        str = str+String(n)
+    }
+    // recursive case
+    else {
+        let lastDigit = n%2
+        let rest = n/2
+        binaryF(n: rest)
+        // print(lastDigit)
+        str = str+String(lastDigit)
+    }
+    return str
+}
+for nbr in 0...8 {
+    print("Le nombre \(nbr) s'écrit en binaire: " +  binaryF(n: nbr))
+    str = ""
+}
+
+
